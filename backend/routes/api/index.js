befacaaeb005e45757cd450b6f82f753dda5cfb6
hai yen <nhyen.93@gmail.com>
2022-08-29 09:05:35 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -53,8 +53,8 @@
 
 // backend/routes/api/index.js
 const router = require("express").Router();
-// const sessionRouter = require('./session.js');
-// const usersRouter = require('./users.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./user.js');
 // const spotRouter = require('./spots.js');
 // const reviewRouter = require('./reviews.js');
 // const bookingRouter = require('./bookings.js');
@@ -65,13 +65,13 @@ const { restoreUser } = require("../../utils/auth.js");
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-// router.use('/session', sessionRouter);
-// router.use('/users', usersRouter);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 // router.use('/spots', spotRouter);
 // router.use('/bookings', bookingRouter);
 // router.use('/reviews', reviewRouter);
 // router.use('/images', imageRouter);
-// router.post('/test', (req, res) => {
-//   res.json({ requestBody: req.body });
-// });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 module.exports = router;
