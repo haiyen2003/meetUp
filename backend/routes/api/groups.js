06459ed17ddd212fef3c70f3e2b684b2efe27d1e
hai yen <nhyen.93@gmail.com>
2022-08-31 22:38:57 -0700

    get all groups by current user

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {Group, Membership, GroupImage} = require('../../db/models');
+const { Group, Membership, GroupImage } = require('../../db/models');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const router = express.Router();
@@ -9,20 +9,46 @@ const membership = require('../../db/models/membership');
 
 //get all Groups
 
-router.get('/', async(req, res, next) =>{
+router.get('/', async (req, res, next) => {
     const groups = await Group.findAll({
-        include:[
-            {model: Membership},
-            {model: GroupImage}
+        include: [
+            { model: Membership },
+            { model: GroupImage }
         ]
     });
     return res.json(groups);
 })
 
+//get all groups by current user (missing numMember)
+router.get('/current', restoreUser, async (req, res, next) => {
+    const { user } = req;
+    if (user) {
+        const currentGroups = await Membership.findAll({
+            where: { userId: user.id },
+            include: {
+                model: Group,
+                attributes:
+                    ['id','organizerId', 'name', 'about', 'type', 'private', 'city', 'state', 'createdAt', 'updatedAt']
+            }
+        })
 
-
-
-
-
+        let result = [];
+        for (let group of currentGroups) {
+            let jsonGroup = group.toJSON();
+            const image = await GroupImage.findOne({
+                where: {
+                    groupId: jsonGroup.groupId
+                }
+            });
+            jsonGroup.Group.previewImage = image.dataValues.url;
+            result.push(jsonGroup);
+        }
+        let resultArr = [];
+        result.forEach(a => resultArr.push(a.Group));
+        return res.json({
+            'Groups': resultArr
+        });
+    }
+})
 
 module.exports = router;
