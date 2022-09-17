41cdb882d722777a51f813ca9945a9845963ee7c
hai yen <nhyen.93@gmail.com>
2022-09-16 22:39:50 -0700

    LoginFormPage setup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,22 @@
+//Redux doesnot let import createStore -> configureStore
+
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+//import * as sessionActions from './store/session';
+
+import sessionReducer from './session';
+
+// if (process.env.NODE_ENV !== 'production') {
+//   restoreCSRF();
+
+//   window.csrfFetch = csrfFetch;
+//   window.store = store;
+//   window.sessionActions = sessionActions;
+// }
+
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session: sessionReducer,
 });
 
 let enhancer;
